09025331d6cf64cadc237eef1038daae56038341
Mack Ramirez <mnrdev510@gmail.com>
2023-11-05 19:38:14 -0800

    customizing theme.colors in tailwind

--- src/Component/ProductDesc.jsx
+++ src/Component/ProductDesc.jsx
@@ -2,19 +2,26 @@ import React from "react";
 
 function ProductDesc() {
   return (
-    <div className="w-80 h-80">
-      <h2>SNEAKER COMPANY</h2>
-      <h1>Fall Limited Edition Sneakers</h1>
-      <p>
+    <div className="text-left pt-32">
+      <h2 className="font-header text-orangePrimary">SNEAKER COMPANY</h2>
+      <h1 className="font-header text-blackNoise text-5xl font-bold">
+        Fall Limited Edition Sneakers
+      </h1>
+      <p className="max-w-lg text-base font-content leading-7 mt-10">
         Thees low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they'll withstand everythinghthe
         weather can offer.
-          </p>
-          <span>$125.00</span>
-          <span>50%</span>
+      </p>
+      <span className="text-blackNoise font-bold text-3xl">$125.00</span>
+      <span className="text-grayishBlue">50%</span>
+      <div className="flex">
+        <div className="bg-lightGrayishBlue flex justify-center items-center w-1/2">
           <span>-</span>
           <span>0</span>
           <span>+</span>
+        </div>
+        <button className="bg-orangePrimary p-5 w-1/2">Add to Cart</button>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      veryDarkBlue: "hsl(220, 13%, 13%)",
+      darkGrayishBlue: "hsl(219, 9%, 45%)",
+      grayishBlue: " hsl(220, 14%, 75%)",
+      lightGrayishBlue: "hsl(223, 64%, 98%)",
+      whiteNoise: "hsl(0, 0%, 100%)",
+      blackNoise: "hsl(0, 0%, 0%)",
+      orangePrimary: "hsl(26, 100%, 55%)",
+      paleOrangePrimary: "hsl(25, 100%, 94%)",
+    },
     extend: {
       container: {
         center: true,
         padding: "1rem",
+      },
+
+      fontFamily: {
+        header: ["Kumbh", 700],
+        content: ["Kumbh", 400],
       },
     },
   },
